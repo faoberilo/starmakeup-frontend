58e89b9eec007067ffabd5d468ef29508dbe4a88
faoberilo <89050385+faoberilo@users.noreply.github.com>
2022-03-08 09:18:05 -0300

    Update ModalCadastroProduto.jsx
    Cadastro de produto funcionando 100%

--- src/components/ModalCadastroProduto/ModalCadastroProduto.jsx
+++ src/components/ModalCadastroProduto/ModalCadastroProduto.jsx
@@ -30,16 +30,26 @@ const ModalCadastroProduto = ({ onClose = () => {}, Children }) => {
     const nome= evento.target.nome.value;
     const descricao= evento.target.descricao.value;      
     const data= evento.target.dataValidade.value;
-    const dataValidade = data[8]+data[9]+"/"+data[5]+data[6]+"/"+data[0]+data[1]+data[2]+data[3] 
+    const diaValidade = parseInt(data[8]+data[9]);
+    const mesValidade = parseInt(data[5]+data[6]);
+    const anoValidade = parseInt(data[0]+data[1]+data[2]+data[3]); 
     const quantidade= parseInt(evento.target.quantidade.value);
     const tipoid = parseInt(evento.target.tipo.value);
+    const precoCusto= parseFloat(evento.target.precoCusto.value);
+    const porcentagemLucro= parseInt(evento.target.porcentagemLucro.value);
+    const promocaodesconto= parseInt(evento.target.promocaodesconto.value);
+    const valorVenda= parseFloat(evento.target.valorVenda.value);
+    const valorAtacado= parseFloat(evento.target.valorAtacado.value);
+    const fornecedorid= parseInt(evento.target.fornecedorid.value);
 
     const produto = {
       codigo,
       imagem,
       nome,
       descricao,      
-      dataValidade,
+      diaValidade,
+      mesValidade,
+      anoValidade,
       quantidade
     };
 
@@ -48,12 +58,38 @@ const ModalCadastroProduto = ({ onClose = () => {}, Children }) => {
       tipoid
     };
 
-    console.log(tipoProduto)
+    const produtoPreco = {
+      codigoid:codigo,
+      precoCusto,
+      porcentagemLucro,
+      promocaodesconto,
+      valorVenda,
+      valorAtacado
+    };
 
-
-    const produtopreco = {};
+    const fornecedorProduto = {
+      produtoid:codigo,
+      fornecedorid,
+    }
 
     axios.post("/produto", produto).then((response) => {});
+
+    setTimeout(() => {
+      axios
+        .post("/produtosprecos", produtoPreco)
+        .catch((response) => {
+          alert(response.message);
+        });
+    }, 500);
+
+    setTimeout(() => {
+      axios
+        .post("/fornecedor-produto", fornecedorProduto)
+        .catch((response) => {
+          alert(response.message);
+        });
+    }, 1000);
+
 
     setTimeout(() => {
       axios
@@ -71,7 +107,7 @@ const ModalCadastroProduto = ({ onClose = () => {}, Children }) => {
         .catch((response) => {
           alert(response.message);
         });
-    }, 1000);
+    }, 1500);
 
         const log = {};
         log.idUser= localStorage.getItem("idUser");
@@ -124,9 +160,9 @@ const ModalCadastroProduto = ({ onClose = () => {}, Children }) => {
 
             <input
               type="text"
-              id="fornecedor"
-              name="fornecedor"
-              placeholder="Fornecedor"
+              id="fornecedorid"
+              name="fornecedorid"
+              placeholder="Código do Fornecedor"
               required/>
             <input
               type="date"
@@ -141,15 +177,15 @@ const ModalCadastroProduto = ({ onClose = () => {}, Children }) => {
                 placeholder="Quantidade"
                 required/>
             <input
-                type="number"
+                type="text"
                 id="precoCusto"
                 name="precoCusto"
                 placeholder="Preço Custo"
                 required/>
             <input
                 type="number"
-                id="porcetagemLucro"
-                name="porcetagemLucro"
+                id="porcentagemLucro"
+                name="porcentagemLucro"
                 placeholder="% Lucro"
                 required/>
             <input
@@ -159,13 +195,13 @@ const ModalCadastroProduto = ({ onClose = () => {}, Children }) => {
                 placeholder="% Promoção"
                 required/>
             <input
-                type="number"
+                type="text"
                 id="valorVenda"
                 name="valorVenda"
                 placeholder="Preço de venda"
                 required/>
             <input
-                type="number"
+                type="text"
                 id="valorAtacado"
                 name="valorAtacado"
                 placeholder="Preço de Atacado"
